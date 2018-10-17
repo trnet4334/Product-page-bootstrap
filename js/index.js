01bf2dbaa3ae339fa7649015aac5079c9897c283
trnet4334 <search0210@gmail.com>
2018-10-16 22:28:34 -0700

    Fix image link lost

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-$(window).scroll(function(evt){
-  if ($(window).scrollTop()>0)
-    $(".navbar").removeClass("navbar-top");
-  else
-      $(".navbar").addClass("navbar-top");
+$(window).scroll(function (evt) {
+    if ($(window).scrollTop() > 0)
+        $(".navbar").removeClass("navbar-top");
+    else
+        $(".navbar").addClass("navbar-top");
 });
 
-var s = skrollr.init();
+let s = skrollr.init();
